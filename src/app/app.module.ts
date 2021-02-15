f231e1f2afb37a534f2233fc9f9810a64642f5c8
KNCY24 <kency.mariema@hotmail.com>
2021-02-15 09:18:32 +0100

    Commit change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductComponent } from './product/product.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/restservice.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { World, Pallier, Product } from './world';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestserviceService {
+  server = "http://localhost:8080/"
+  user = "";
+
+  constructor(private http: HttpClient) { }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+   }
+   getWorld(): Promise<World> {
+    return this.http.get(this.server + "adventureisis/generic/world")
+    .toPromise().catch(this.handleError);
+   };
+
+   
+  getUser() : string {
+    return ""+this.user;
+  }
+
+  setUser(username : string) : void {
+    this.user = username;
+  }
+
+}
+
+
