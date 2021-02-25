37debcb510e5771690b541574987bf233bb3e35e
KNCY24 <kency.mariema@hotmail.com>
2021-02-25 23:18:37 +0100

    buy function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   timeleft : number[] = [0,0,0,0,0,0];
   lastupdate : number[]= [0,0,0,0,0,0];
   commutateur : number = 1; //1, 10, 100, 0(max)
+  multi : number[]=[1,10,100];
   qtmulti : number[] = [1,1,1,1,1,1];
 
   constructor(private service: RestserviceService) { 
@@ -25,9 +26,22 @@ export class AppComponent {
   }
 
   startFabrication(p : number){
-    this.timeleft[p] = this.world.products.product[p].timeleft;
-    this.lastupdate[p] = Date.now();
-    this.world.products.product[p].cout = this.world.products.product[p].cout * this.world.products.product[p].croissance;
+    if(this.world.products.product[p].cout <= this.world.money && this.timeleft[p]<=0){
+      let multiplicateur = 0;
+      let price = 0;
+      for(let n=0;n<this.qtmulti[p];n++){
+        multiplicateur = multiplicateur + (1 * Math.pow(this.world.products.product[p].croissance,n));
+      } 
+      price = this.world.products.product[p].cout*multiplicateur;
+      
+      if(price <= this.world.money){
+        this.world.money = this.world.money - price;
+      
+        this.timeleft[p] = this.world.products.product[p].timeleft;
+        this.lastupdate[p] = Date.now();
+        this.world.products.product[p].cout = this.world.products.product[p].cout * this.world.products.product[p].croissance;
+      }
+    }    
   }
 
   
@@ -43,6 +57,7 @@ export class AppComponent {
         if(this.timeleft[i] <= 0){
           this.timeleft[i] = 0;
           this.progressbarvalue[i]=0;
+          this.world.products.product[i].quantite = this.world.products.product[i].quantite+this.qtmulti[i];
           this.world.score = this.world.score + this.world.products.product[i].revenu;
           this.world.money = this.world.money + this.world.products.product[i].revenu;
           this.calcMaxCanBuy();
@@ -56,7 +71,19 @@ export class AppComponent {
 
   calcMaxCanBuy() {
     for(let i=0; i<6 ;i++) {
-
+      this.qtmulti[i]=1;
+      let multiplicateur = 0;
+      let price = 0;
+      for(let j=0;j<3;j++){
+        for(let n=0;n<this.multi[j];n++){
+          multiplicateur = multiplicateur + (1 * Math.pow(this.world.products.product[i].croissance,n));
+        } 
+        price = this.world.products.product[i].cout*multiplicateur;
+        if(price <= this.world.money) {
+          this.qtmulti[i] = this.multi[j];
+        }
+        multiplicateur=0;
+      }
     }
   }
 }
