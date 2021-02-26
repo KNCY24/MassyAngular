3076cef43fda184b0740d73425f809d719470753
Emma <emma.rousseau@etud.univ-jfc.fr>
2021-02-26 08:21:43 +0100

    pipe angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { ProductComponent } from './product/product.component';
 import {MatProgressBarModule} from '@angular/material/progress-bar'
 
 import { HttpClientModule } from '@angular/common/http';
+import { BigvaluePipe } from './bigvalue.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    BigvaluePipe
   ],
   imports: [
     BrowserModule,
